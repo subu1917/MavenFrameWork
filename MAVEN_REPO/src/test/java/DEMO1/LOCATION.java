c9a9e914c85fa9ec8e3354a13d2ebea321916f85
Win10 <Win10@DESKTOP-VUC6R45>
2024-07-21 21:03:50 +0530

    first maven push1

---
+++ MAVEN_REPO/src/test/java/DEMO1/LOCATION.java
@@ -0,0 +1,13 @@
+package DEMO1;
+
+public class LOCATION {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	System.out.println("my location:-puri");
+	
+	
+	}
+
+}
---
+++ MAVEN_REPO/src/test/java/DEMO1/NAME.java
@@ -0,0 +1,13 @@
+package DEMO1;
+
+public class NAME {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	System.out.println("my name is:-subu");
+	
+	
+	}
+
+}
